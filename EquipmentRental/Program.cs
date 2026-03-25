a2e672ebf5914f3a994a6c96dda9cafb1c8fca1c
Demian Kishyk <s34175@pjwstk.edu.pl>
2026-03-25 23:16:28 +0100

    feat: add demo scenario in Program.cs

--- EquipmentRental/Program.cs
+++ EquipmentRental/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using EquipmentRental.Services;
+
+var service = new RentalService();
+
+var laptop = new Laptop { Name = "Dell", Ram = 16, Cpu = "i7" };
+var student = new Student { FirstName = "Jan", LastName = "Kowalski" };
+
+service.Rent(laptop, student);
+service.Return(laptop);
