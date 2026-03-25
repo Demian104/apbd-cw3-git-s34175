eab487d0730db898f2ebaada4ea882fc25ad9e31
Demian Kishyk <s34175@pjwstk.edu.pl>
2026-03-25 22:56:12 +0100

    feat: add equipment types (Laptop, Camera, Projector)

--- EquipmentRental/Models/Equipment.cs
+++ EquipmentRental/Models/Equipment.cs
@@ -4,3 +4,21 @@
     public string Name { get; set; }
     public bool IsAvailable { get; set; } = true;
 }
+
+public class Laptop : Equipment
+{
+    public int Ram { get; set; }
+    public string Cpu { get; set; }
+}
+
+public class Camera : Equipment
+{
+    public int Resolution { get; set; }
+    public bool HasFlash { get; set; }
+}
+
+public class Projector : Equipment
+{
+    public int Lumens { get; set; }
+    public string Technology { get; set; }
+}
