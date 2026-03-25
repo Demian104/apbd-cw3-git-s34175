3c3407db540eef9bd7e58f3d7882bc79a6e02000
Demian Kishyk <s34175@pjwstk.edu.pl>
2026-03-25 22:57:05 +0100

    feat: add user hierarchy (User, Student, Employee)

---
+++ EquipmentRental/Models/User.cs
@@ -0,0 +1,14 @@
+﻿public abstract class User
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+public class Student : User
+{
+}
+
+public class Employee : User
+{
+}
