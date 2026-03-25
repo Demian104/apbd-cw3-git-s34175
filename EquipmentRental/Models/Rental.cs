87283eff5eea1a26ce522caea7df67c967d0f84a
Demian Kishyk <s34175@pjwstk.edu.pl>
2026-03-25 22:58:18 +0100

    feat: add Rental entity

---
+++ EquipmentRental/Models/Rental.cs
@@ -0,0 +1,8 @@
+﻿public class Rental
+{
+    public Equipment Equipment { get; set; }
+    public User User { get; set; }
+    public DateTime RentDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
+}
